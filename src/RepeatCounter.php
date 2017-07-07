215d88f293558f9ddb063d2bbc001221700ec74a
Calla Rudolph <callarudolph@gmail.com>
2017-07-07 09:55:32 -0700

    pass second test that keeps score of number of matches

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,8 +4,19 @@
     {
         function countRepeats($base_input, $string_input)
         {
-            if ($base_input == $string_input)
-                return 1;
+            $string_to_word = explode(" ", $string_input);
+            $matches = [];
+
+            foreach($string_to_word as $word)
+            {
+                if ($base_input == $word) {
+                    $match = 1;
+                    array_push($matches, $match);
+                }
+            }
+
+            $number_of_matches = array_sum($matches);
+            return $number_of_matches;
         }
     }
 
