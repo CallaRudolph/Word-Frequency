645102b8caaa6cf8742ff8432ff6ea4b17cbc3b1
Calla Rudolph <callarudolph@gmail.com>
2017-07-07 11:18:47 -0700

    pass fourth test to check for spaces in base input

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,21 +4,26 @@
     {
         function countRepeats($base_input, $string_input)
         {
+            $space_check = " ";
             $base_lower = strtolower($base_input);
             $string_lower = strtolower($string_input);
             $string_to_word = explode(" ", $string_lower);
             $matches = [];
 
-            foreach($string_to_word as $word)
-            {
-                if ($base_lower == $word) {
-                    $match = 1;
-                    array_push($matches, $match);
+            if (strpos($base_input, $space_check) == true) {
+                $error = "Base input can only be one word";
+                return $error;
+            } else {
+                foreach($string_to_word as $word)
+                {
+                    if ($base_lower == $word) {
+                        $match = 1;
+                        array_push($matches, $match);
+                    }
                 }
+                $number_of_matches = array_sum($matches);
+                return $number_of_matches;
             }
-
-            $number_of_matches = array_sum($matches);
-            return $number_of_matches;
         }
     }
 
