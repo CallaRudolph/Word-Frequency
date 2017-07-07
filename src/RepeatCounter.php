f0787aa2015d5f70ebbd7b8e500a67d982f2ff98
Calla Rudolph <callarudolph@gmail.com>
2017-07-07 10:04:21 -0700

    pass third test to convert all inputs to lowercase

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,12 +4,14 @@
     {
         function countRepeats($base_input, $string_input)
         {
-            $string_to_word = explode(" ", $string_input);
+            $base_lower = strtolower($base_input);
+            $string_lower = strtolower($string_input);
+            $string_to_word = explode(" ", $string_lower);
             $matches = [];
 
             foreach($string_to_word as $word)
             {
-                if ($base_input == $word) {
+                if ($base_lower == $word) {
                     $match = 1;
                     array_push($matches, $match);
                 }
