6c8fc52eca882394cd1da4db81034f07a446f9c2
Calla Rudolph <callarudolph@gmail.com>
2017-07-07 12:40:08 -0700

    create specialCharacters method with boolean to check second input for 5th spec

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -2,15 +2,34 @@
 
     class RepeatCounter
     {
+        function specialCharacters($string_input)
+        {
+            $special_check = false;
+            $string_to_word = explode(" ", $string_input);
+            foreach($string_to_word as $word)
+            {
+                if (ctype_alpha($word) == false)
+                {
+                    $special_check = true;
+                }
+            }
+            return $special_check;
+        }
+
         function countRepeats($base_input, $string_input)
         {
+            $alpha_check = $this->specialCharacters($string_input);
             $space_check = " ";
             $base_lower = strtolower($base_input);
             $string_lower = strtolower($string_input);
             $string_to_word = explode(" ", $string_lower);
             $matches = [];
 
-            if (strpos($base_input, $space_check) == true) {
+            var_dump($alpha_check);
+            if ($alpha_check == true) {
+                $special_error = "String of words cannot contain special characters";
+                return $special_error;
+            } elseif (strpos($base_input, $space_check) == true) {
                 $error = "Base input can only be one word";
                 return $error;
             } else {
